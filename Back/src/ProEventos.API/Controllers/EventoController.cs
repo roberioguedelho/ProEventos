8ed910d597125dd18ae31cc18a2eddbdc974a342
Robério Guedelho <roberio@vizir.com.br>
2022-05-17 14:20:57 -0300

    feat: add evento model

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.models;
 
 namespace ProEventos.API.Controllers
 {
@@ -15,10 +16,43 @@ namespace ProEventos.API.Controllers
         {
         }
 
+        public IEnumerable<Evento> _evento = new Evento[] {
+                new Evento() {
+                    EventoId = 1,
+                    Tema = "Angular 11 e .NET 5",
+                    Local = "BH",
+                    Lote = "1° Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                    ImagemUrl = "foto.png"
+                },
+                new Evento() {
+                    EventoId = 2,
+                    Tema = "Angular 13 e .NET 6",
+                    Local = "BH",
+                    Lote = "1° Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                    ImagemUrl = "foto.png"
+                }
+        };
+
         [HttpGet]
-        public String Get()
+        public IEnumerable<Evento> Get()
         {
-            return "value";
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+        [HttpPost]
+        public String Post()
+        {
+            return "Exemplo post";
         }
     }
 }
